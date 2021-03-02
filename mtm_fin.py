ad7060fb5956c44f8a22c73f2579b2300011ecb4
Victor <79273705+victorscarpes@users.noreply.github.com>
2021-03-02 12:59:23 -0300

    Mudança da convenção das taxas
    Agora, todas as funções vão trabalhar com as taxas em notação não percentual. Uma taxa de 10% antes seria representada como i=10, mas agora vai ser representada como i=0.1.

--- mtm_fin.py
+++ mtm_fin.py
@@ -19,10 +19,10 @@ def F_P(i, n):
     Exemplo: dado um valor presente de R$ 1.000,00 que rende a 10% a.m. por 5 meses,
     o valor do capital total ao final desses 5 meses vai ser dado pela seguinte equacao:
     
-    F = 1000 * F_P(i=10, n=5)
+    F = 1000 * F_P(i=0.1, n=5)
     """
 
-    return (1+i*0.01)**n
+    return (1+i)**n
 
 def P_F(i, n):
 
@@ -32,10 +32,10 @@ def P_F(i, n):
     Exemplo: dado um valor futuro de R$ 1.500,00 que rendeu a 5% a.a. por 2 anos,
     o valor do capital investido inicialmente vai ser dado pela seguinte equacao:
     
-    P = 1500 * P_F(i=5, n=2)
+    P = 1500 * P_F(i=0.05, n=2)
     """
 
-    return 1/((1+i*0.01)**n)
+    return 1/((1+i)**n)
 
 def F_A(i, n, ante=False):
 
@@ -45,16 +45,16 @@ def F_A(i, n, ante=False):
     Exemplo: dado uma serie de investimentos mensais de R$ 500,00 com um rendimento de 4% a.m. por 10 meses,
     o valor do capital total final vai ser dado pela seguinte equacao (assumindo pagamentos no final de cada periodo):
     
-    F = 500 * F_A(i=4, n=10)
+    F = 500 * F_A(i=0.04, n=10)
     
     Caso o pagamento seja no inicio de cada periodo (antecipado):
 
-    F = 500 * F_A(i=4, n=10, ante=True)
+    F = 500 * F_A(i=0.04, n=10, ante=True)
     """
 
     if ante == False:
-        return ((1+i*0.01)**n -1)/(i*0.01)
-    return ((1+i*0.01)**n -1)/(i*0.01) * (1+i*0.01)
+        return ((1+i)**n -1)/i
+    return ((1+i)**n -1)/i * (1+i)
 
 def P_A(i, n, ante=False):
 
@@ -64,16 +64,16 @@ def P_A(i, n, ante=False):
     Exemplo: dado uma serie de pagamentos anuais de R$ 100,00 com um rendimento de 8% a.a. por 5 anos,
     o valor do capital inicial vai ser dado pela seguinte equacao (assumindo pagamentos no final de cada periodo):
     
-    P = 100 * P_A(i=8, n=5)
+    P = 100 * P_A(i=0.08, n=5)
     
     Caso o pagamento seja no inicio de cada periodo (antecipado):
 
-    P = 100 * P_A(i=8, n=5, ante=True)
+    P = 100 * P_A(i=0.08, n=5, ante=True)
     """
 
     if ante == False:
-        return ((1+i*0.01)**n -1)/(i*0.01*((1+i*0.01)**n))
-    return ((1+i*0.01)**n -1)/(i*0.01*((1+i*0.01)**(n-1)))
+        return ((1+i)**n -1)/(i*((1+i)**n))
+    return ((1+i)**n -1)/(i*((1+i)**(n-1)))
 
 def A_F(i, n, ante=False):
 
@@ -83,16 +83,16 @@ def A_F(i, n, ante=False):
     Exemplo: dado um valor futuro de R$ 100.000,00 com um rendimento de 0,5% a.s. por 3 semestres,
     o valor de cada pagamento semestral vai ser dado pela seguinte equacao (assumindo pagamentos no final de cada periodo):
     
-    A = 100000 * A_F(i=0.5, n=3)
+    A = 100000 * A_F(i=0.005, n=3)
     
     Caso o pagamento seja no inicio de cada periodo (antecipado):
 
-    A = 100000 * A_F(i=0.5, n=3, ante=True)
+    A = 100000 * A_F(i=0.005, n=3, ante=True)
     """
 
     if ante == False:
-        return (i*0.01)/((1+i*0.01)**n -1)
-    return (i*0.01)/((1+i*0.01)**n -1) * 1/(1+i*0.01)
+        return (i)/((1+i)**n -1)
+    return (i)/((1+i)**n -1) * 1/(1+i)
 
 def A_P(i, n, ante=False):
 
@@ -102,16 +102,16 @@ def A_P(i, n, ante=False):
     Exemplo: dado um valor pesente de R$ 5.000,00 com um rendimento de 1,25% a.b. por 5 bimestres,
     o valor de cada pagamento bimestral vai ser dado pela seguinte equacao (assumindo pagamentos no final de cada periodo):
     
-    A = 5000 * A_P(i=1.25, n=5)
+    A = 5000 * A_P(i=0.00125, n=5)
     
     Caso o pagamento seja no inicio de cada periodo (antecipado):
 
-    A = 5000 * A_P(i=1.25, n=5, ante=True)
+    A = 5000 * A_P(i=0.00125, n=5, ante=True)
     """
 
     if ante == False:
-        return (i*0.01*((1+i*0.01)**n))/((1+i*0.01)**n -1)
-    return (i*0.01*((1+i*0.01)**(n-1)))/((1+i*0.01)**n -1)
+        return (i*((1+i)**n))/((1+i)**n -1)
+    return (i*((1+i)**(n-1)))/((1+i)**n -1)
 
 def taxa_equivalente(i=1, n=1):
 
@@ -119,16 +119,16 @@ def taxa_equivalente(i=1, n=1):
     Converte uma taxa efetiva de uma periodicidade para uma taxa efetiva equivalente em outra periodicidade
     
     Exemplo: temos uma taxa de 15% a.m e queremos calcular a taxa que quando capitalizada anualmente resulta no mesmo rendimento.
-    Nesse caso, temos i=15. Como a cabem 12 periodos da periodicidade antiga em um periodo da periodicidade desejada
+    Nesse caso, temos i=0.15. Como a cabem 12 periodos da periodicidade antiga em um periodo da periodicidade desejada
     (12 meses em um ano), temos n=12.
     
     Exemplo: temos uma taxa de 10% a.m e queremos calcular a taxa que quando capitalizada diariamente (mes padrão de 30 dias)
     resulta no mesmo rendimento.
-    Nesse caso, temos i=10. Como a cabem 1/30 periodos da periodicidade antiga em um periodo da periodicidade desejada
+    Nesse caso, temos i=0.1. Como a cabem 1/30 periodos da periodicidade antiga em um periodo da periodicidade desejada
     (cabem 1/30 meses em um dia), temos n=1/30
     """
 
-    return ((1+i*0.01)**n-1)*100
+    return (1+i)**n-1
 
 def taxa_efetiva_equivalente(i=1, n=1):
 
@@ -137,10 +137,10 @@ def taxa_efetiva_equivalente(i=1, n=1):
     
     Exemplo: temos uma aplicacao que rende a 10% a.a. capitalizada mensalmente e queremos calcular qual taxa anual que,
     quando capitalizada anualmente resulta no mesmo rendimento.
-    Vamos ter, nesse caso, i=10. Como o periodo da taxa (um ano) equivale a 12 vezes o periodo de capitalizacao (um mes), temos n=12.
+    Vamos ter i=0.1 nesse caso. Como o periodo da taxa (um ano) equivale a 12 vezes o periodo de capitalizacao (um mes), temos n=12.
     """
 
-    return 100*((1 + (i*0.01)/n)**n -1)
+    return (1 + (i)/n)**n -1
 
 def taxa(P=None, F=None, A=0, n=0, ante=False):
 
@@ -186,17 +186,17 @@ def periodos(P=None, F=None, A=0, i=0, ante=False):
     Exemplo: se um investimento inicial de R$ 1.000,00 a uma taxa de 10% a.m. resultou em um valor futuro de R$ 5.000,00
     sem pagamentos periodicos, qual foi a duracao n em meses do investimento?
 
-    n = periodos(P=1000, F=5000, i=10)
+    n = periodos(P=1000, F=5000, i=0.1)
 
     Exemplo: se um investimento anual de R$ 500,00 ao final de cada ano a uma taxa de 12% a.a. resultou
     em um valor futuro de R$ 10.000,00, quantos anos durou o investimento?
 
-    n = periodos(A=500, F=10000, i=12)
+    n = periodos(A=500, F=10000, i=0.12)
 
     Exemplo: se uma empresa que trabalha com taxas de 15,25% a.m. vende um produto de R$12.000,00 para ser pago em parcelas mensais de R$ 500,00
     com a primeira parcela sendo paga no ato da venda, qual é a quantidade de parcelas?
 
-    n = periodos(P=12000, A=500, i=15.25, ante=True)
+    n = periodos(P=12000, A=500, i=0.1525, ante=True)
     """
 
     if (P != None and F != None and A in (None, 0)):
